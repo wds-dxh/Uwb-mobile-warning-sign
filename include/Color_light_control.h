0e38535860a137aaead18a4aa654afeac0ae2e15
dxhwds <94962243+dxhwds@users.noreply.github.com>
2023-09-19 19:12:31 +0800

    “”

--- include/Color_light_control.h
+++ include/Color_light_control.h
@@ -12,12 +12,12 @@ public:
     void color_light_control_init(uint8_t luminance);  // 初始化灯光控制
     void color_control(uint16_t Num,char color);  // 控制单个灯光灯光
     void color_off(uint16_t Num);  // 关闭单个灯光
-
-    void color_riot_of_colours(uint8_t quantity);  // 七彩灯
-    void color_control_all(char color);  // 控制全部灯光
-    void color_flash(uint8_t quantity_1,char color);  // 闪烁灯光
-
     void color_light_all_every(int R,int G,int B);  // 全部灯光亮度调节
+    void color_control_all(char color);  // 控制全部灯光
+
+    void color_flash(uint8_t quantity_1,char color);  // 闪烁灯光
+    void color_riot_of_colours(uint8_t quantity);  // 七彩灯
+    
 
 
 
